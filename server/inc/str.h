2e1cf31e89bd96645c837a6f336e21d04fa59022
TheArcanum <joan@Arcanum.(none)>
2015-05-01 22:28:04 +0200

    Create folder server

---
+++ server/inc/str.h
@@ -0,0 +1,12 @@
+#ifndef __STR__
+#define __STR__
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/* Renvoie 1 si la chaine w commence par s */
+int str_beginwith(const char *w,const char *s);
+
+
+#endif
