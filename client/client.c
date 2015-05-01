1a72525c80df09313766bb8ca5a624c0379da092
Horloge-Skynet <sam.forestier@gmail.com>
2015-05-01 22:51:00 +0200

    Create client.c

---
+++ client/client.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+#include "string.h"
+#include "unistd.h"
+#include "netdb.h"
+#include "sys/socket.h"
+#include "sys/types.h"
+#include "arpa/inet.h"
+#include "netinet/in.h"
+
+
+#define DOMAIN AF_INET
+#define PORT   YOURPORT
+
+
+int main(int argc, const char* argv[])
+{
+  return 0;
+}
