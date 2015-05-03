8908a46a15c21c67e925d4246fb6eb5f28294567
Horloge-Skynet <sam.forestier@gmail.com>
2015-05-03 22:27:56 +0200

    Update server.h

--- server/inc/server.h
+++ server/inc/server.h
@@ -15,7 +15,7 @@
 #define MAX_CLIENTS	4
 #define FREE 		1
 #define TAKEN		2
-#define SID			-1
+#define SID		-1
 
 #define RUNNING		1
 #define	STOPPING	2
@@ -23,8 +23,8 @@
 struct client_t
 {
 	struct sockaddr_in addr;
-	int sock;
 	unsigned int id;
+	int sock;
 	int status;
 };
 
@@ -34,13 +34,12 @@ unsigned int count;
 pthread_t threads[MAX_CLIENTS];
 pthread_t command_thread;
 
-int 	isvalid(const char *);
-void 	broadcast(int, const char *);
+int 	isvalid(const char*);
+void 	broadcast(int, const char*);
 void 	close_all_connections();
 int 	process_command(const char*, int);
 void* 	command_handler(void* data);
 void* 	on_connection(void *data);
-int		start_server(void);
+int	start_server(void);
 
 #endif
-
