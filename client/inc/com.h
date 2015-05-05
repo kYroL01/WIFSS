0a56b574010a12904171cd9a01bee0e5752b3b51
Arcanum <c5h11n2o2p@gmail.com>
2015-05-05 17:51:37 +0200

    Create com.h

---
+++ client/inc/com.h
@@ -0,0 +1,14 @@
+#ifndef __COM_H
+#define __COM_H
+
+#define OK        "ok"
+#define END       "quit"
+#define PRESENT   "present"
+#define FINISHED  "finished"
+#define ISPRESENT "ispresent"
+
+#define ENDSIG  -1
+#define ASKFILE  1
+#define FILEDWL  2
+
+#endif
