5e56ee775fbcb80d7749879c0f8ae7b105c19b0b
Horloge-Skynet <sam.forestier@gmail.com>
2015-05-05 19:58:44 +0200

    Update com.h

--- client/inc/com.h
+++ client/inc/com.h
@@ -1,8 +1,10 @@
 #ifndef __COM_H
 #define __COM_H
 
+
 #define OK        "ok"
 #define END       "quit"
+#define LIST      "list"
 #define PRESENT   "present"
 #define FINISHED  "finished"
 #define ISPRESENT "ispresent"
@@ -10,5 +12,7 @@
 #define ENDSIG  -1
 #define ASKFILE  1
 #define FILEDWL  2
+#define ASKLIST  3
+
 
 #endif
