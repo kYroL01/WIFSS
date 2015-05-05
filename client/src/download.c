a09027b56f5ceb550afd18c39d1811a95284ce58
Arcanum <c5h11n2o2p@gmail.com>
2015-05-05 17:11:55 +0200

    Create download.c

---
+++ client/src/download.c
@@ -0,0 +1,45 @@
+#include <client.h>
+
+int download(char *path, int sock)
+{
+	FILE *__file = NULL;
+
+	if(!access(path, F_OK)) //Check existence of the file before download one with the same name
+	{
+		__file = fopen(path, "wb");
+	}
+
+
+	if(__file == NULL)
+	{
+		printf("%s file cannot be created.\n", path);
+	}
+
+	else
+	{
+		printf("Reception of %s file started.\n", path);
+
+		char __buff[BUFFER];
+		rewind(__file);
+
+		bool keepGoing = true;
+		while(keepGoing)
+		{
+			while(recv(sock, __buff, BUFFER, false) != BUFFER)
+
+			if(!strcmp(__buff, FINISHED))
+			{
+				keepGoing = false;
+			}
+			else
+			{
+				fwrite(__buff, BUFFER, BUFFER, __file);
+			}
+
+			fseek(__file, SEEK_CUR, SEEK_CUR + BUFFER);
+			memset(__buff, 0, BUFFER);
+		}
+
+		fclose(__file);
+	}
+}
