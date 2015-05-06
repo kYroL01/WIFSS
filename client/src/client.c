92b1eb75bec7dfafad63b872a5ec2169bbcba66d
Horloge-Skynet <sam.forestier@gmail.com>
2015-05-06 22:45:17 +0200

    Update client.c

--- client/src/client.c
+++ client/src/client.c
@@ -4,49 +4,21 @@
 
 int main(int argc, char const *argv[])
 {
-	char buff[BUFFER];
 	short int port;
-
-	printf("\n\033[32mStarting client...\033[0m\n");
-
-	printf("\n\033[1;0mServer IP: \033[0m");
-	fgets(buff, BUFFER, stdin);
-
-	do
-	{
-		printf("\033[1;0mPort: \033[0m");
-		scanf("%hd", &port);
-
-	} while(port > 65535 || port < 1024);
-
 	struct sockaddr_in SERVER;
-	SERVER.sin_family      = AF_INET;
-	SERVER.sin_port        = htons(port);
-	SERVER.sin_addr.s_addr = inet_addr(buff);
 
-	int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	initialisation(&SERVER, &port);
 	
-	short int value = 1;
-	while(value != 0)
-	{
-		value = connect(sock, (struct sockaddr*)&SERVER, sizeof(SERVER));
-
-		if(value != 0)
-		{
-			printf("\n\n\033[31mError while connecting to %s:%d.\nLet's retry.\033[0m\n\n", buff, port);
-			sleep(3);
-		}
-	}
 	
 	bool keepGoing = true;
 	while(keepGoing)
 	{
-		memset(buff, 0, BUFFER);
+		/*memset(buff, 0, BUFFER); //Dialogue basique
 
 		printf("|: ");
 		fgets(buff, BUFFER, stdin);
 
-		send(sock, buff, BUFFER, 0);
+		send(sock, buff, BUFFER, 0);*/
 
 		if(!strcmp(buff, END))
 		{
