146775e16b34b2c53df52931528d1458a6faf960
Horloge-Skynet <sam.forestier@gmail.com>
2015-05-08 12:50:56 +0200

    Update upload.c

--- client/src/upload.c
+++ client/src/upload.c
@@ -4,41 +4,41 @@
 
 int upload(const char *path, int idClient, int sock)
 {
-	if(!s_Com(ASKFILE, sock, path, indClient))
+	if(!s_Com(ASKFILE, sock, path, idClient))
 	{
 		printf("Client %d does\'nt have %s file...\n", idClient, path);
 		return -1;
 	}
 
-	FILE *__file = NULL;
+	FILE *_file = NULL;
 
-	uint32_t __fsize = 0;
+	uint32_t _fsize = 0;
 
 	printf("Trying to upload %s to Server...\n", path);
-	__file = fopen(path, "rb");
+	_file = fopen(path, "rb");
 
-	if(__file == NULL)
+	if(_file == NULL)
 	{
 		printf("%s file asked is unreachable.\n", path);
 		return -1;
 	}
 
-	fseek(__file, 0, SEEK_END);
-	__fsize = ftell(__file);
-	fseek(__file, 0, 0);
+	fseek(_file, 0, SEEK_END);
+	_fsize = ftell(_file);
+	fseek(_file, 0, 0);
 
-	printf("Sending: %d bytes of %s\n", __fsize, path);
+	printf("Sending: %d bytes of %s\n", _fsize, path);
 
-	char __buff[BUFFER];
+	char _buff[BUFFER];
 
-	while(ftell(__file) != SEEK_END)
+	while(ftell(_file) != SEEK_END)
 	{
-		memset(__buff, 0, BUFFER);
+		memset(_buff, 0, BUFFER);
 
-		fread(__buff, sizeof(char), BUFFER, __file);
+		fread(_buff, sizeof(char), BUFFER, _file);
 
-		while(send(sock, __buff, strlen(__buff), false) != strlen(__buff));
+		while(send(sock, _buff, strlen(_buff), false) != strlen(_buff));
 	}
 
-	fclose(__file);
+	fclose(_file);
 }
