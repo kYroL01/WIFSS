95a7ddfed44a0088782e4bd62fb606339a6e512b
Horloge-Skynet <sam.forestier@gmail.com>
2015-05-08 12:50:31 +0200

    Update initialisation.c

--- client/src/initialisation.c
+++ client/src/initialisation.c
@@ -2,37 +2,71 @@
 
 
 
-void initialisation(struct sockaddr_in *SERVER, short int *sock)
+void initialisation(struct sockaddr_in *SERVER, int *sock, bool *connected)
 {
-	char __buff[BUFFER];
-	short int __port;
+	char _buff[BUFFER];
+	short int _port;
 
 	printf("\n\033[32mStarting client...\033[0m\n");
+	sleep(1);
 
 	printf("\nServer IP: ");
-	fgets(__buff, BUFFER, stdin);
+	scanf("%s", _buff);
+	getchar();
 
 	do
 	{
-		printf("\nPort: ");
-		scanf("%hd", &__port);
+		printf("Port: ");
+		scanf("%hd", &_port);
 
-	} while(__port > 65535 || __port < 1024);
+	} while(_port > 65535 || _port < 1024);
 
 	SERVER->sin_family      = AF_INET;
-	SERVER->sin_port        = htons(__port);
-	SERVER->sin_addr.s_addr = inet_addr(__buff);
+	SERVER->sin_port        = htons(_port);
+	SERVER->sin_addr.s_addr = inet_addr(_buff);
 
 	*sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	
-	short int result;
+	int result;
+	short int _i;
 	do
 	{
 		result = connect(*sock, (struct sockaddr*)SERVER, sizeof(*SERVER));
-		printf("\n\n\033[31mError while connecting to %s:%hd.\nLet's retry.\033[0m\n\n", inet_ntoa(SERVER->sin_addr.s_addr), port);
-		sleep(3);
 
-	} while(result == -1);
+		if(result == -1)
+		{
+			printf("\n\n\033[31mError while connecting to %s:%hd.\nDo you want retry ? (yes / no)\033[0m\n:| ", inet_ntoa(SERVER->sin_addr), _port);
+			scanf("%s", _buff);
+			getchar();
 
-	printf("\nConnected to %s:%hd.\n\n", inet_ntoa(SERVER.sin_addr.s_addr), port);
+			for(_i = 0; _i < strlen(_buff); _i++) { _buff[_i] = tolower(_buff[_i]); }
+
+			if(!strcmp(_buff, "no") || !strcmp(_buff, "n"))
+			{
+				*connected = false;
+				return;
+			}
+		}
+
+		else
+		{
+			*connected = true;
+			printf("\nConnected to %s:%hd.\n\n", inet_ntoa(SERVER->sin_addr), ntohs(SERVER->sin_port));
+		}
+
+	} while(result == -1 && (!strcmp(_buff, "yes") || !strcmp(_buff, "y")));
+}
+
+
+void disconnection(int sock)
+{
+	if(close(sock) == -1)
+	{
+		printf("\n\033[35mSocket couldn't be successfully closed.\033[0m\n");
+	}
+
+	printf("\n\n\033[35mSocket successfully closed.\033[0m\n\n");
+	sleep(1);
+	printf("WIFFS Client is shutting down for now !\n\n\n\n");
+	sleep(1);
 }
